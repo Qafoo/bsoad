7744dd93bdff8705cde530b74603559b70f7ac72
Kore Nordmann <mail@kore-nordmann.de>
2012-04-19 15:29:28 +0200

    Stubbed test

---
+++ test/Qafoo/Bsoad/Reader/TSharkTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of Bsoad
+ *
+ * @version $Revision$
+ * @license https://www.gnu.org/licenses/agpl-3.0.txt AGPL
+ */
+
+namespace Qafoo\Bsoad;
+
+/**
+ * @version $Revision$
+ *
+ * @covers \Puppeteer\Commons\Struct
+ * @group provider
+ * @group unittest
+ */
+class TSharkTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue( true );
+    }
+}
