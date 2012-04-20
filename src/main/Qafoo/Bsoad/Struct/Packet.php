35a0a8c314e9df006c68a0dceeabcfbf4744bd36
Kore Nordmann <mail@kore-nordmann.de>
2012-04-20 15:15:50 +0200

    Document packet struct

--- src/main/Qafoo/Bsoad/Struct/Packet.php
+++ src/main/Qafoo/Bsoad/Struct/Packet.php
@@ -16,24 +16,89 @@ use Qafoo\Bsoad\Struct;
  */
 class Packet extends Struct
 {
+    /**
+     * Packet timestamp
+     *
+     * @var float
+     */
     public $timestamp;
 
+    /**
+     * Frame number
+     *
+     * @var int
+     */
     public $frame;
+
+    /**
+     * Source host
+     *
+     * @var string
+     */
     public $srcHost;
+
+    /**
+     * Destination host
+     *
+     * @var string
+     */
     public $dstHost;
 
+    /**
+     * Source port
+     *
+     * @var int
+     */
     public $tcpSrcPort;
+
+    /**
+     * Destination port
+     *
+     * @var int
+     */
     public $tcpDstPort;
+
+    /**
+     * TCP sequence number
+     *
+     * @var int
+     */
     public $tcpSequence;
+
+    /**
+     * TCP package length
+     *
+     * @var int
+     */
     public $tcpLength;
+
+    /**
+     * TCP flags
+     *
+     * @var int
+     */
     public $tcpFlags;
+
+    /**
+     * TCP flags description
+     *
+     * @var string
+     */
     public $tcpFlagsShow;
 
-    public $headers = array();
-    public $body;
+    /**
+     * Package data
+     *
+     * @var string
+     */
+    public $data;
 
+    /**
+     * Processing status
+     *
+     * @var mixed
+     */
     public $queued = false;
-    public $processed = false;
 
     /**
      * Get string representation of packet
@@ -43,7 +108,7 @@ class Packet extends Struct
     public function __toString()
     {
         $time = new \DateTime( '@' . floor( $this->timestamp ) );
-        return sprintf( "[%s] % 4d %s:% 5d -> %s:% 5d (% 5d +% 5d) % 8s %s %s\n",
+        return sprintf( "[%s] % 4d %s:% 5d -> %s:% 5d (% 5d +% 5d) % 8s %s\n",
             $time->format( 'r' ),
             $this->frame,
             $this->srcHost,
@@ -53,8 +118,7 @@ class Packet extends Struct
             $this->tcpSequence,
             $this->tcpLength,
             decbin( $this->tcpFlags ),
-            $this->tcpFlagsShow,
-            reset( $this->headers )
+            $this->tcpFlagsShow
         );
     }
 }
