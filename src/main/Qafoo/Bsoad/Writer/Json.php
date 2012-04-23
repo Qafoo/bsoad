881d177307bc14253521be8e7267028050b8b138
Kore Nordmann <mail@kore-nordmann.de>
2012-04-23 16:47:38 +0200

    Implemented and use JSON writer

--- src/main/Qafoo/Bsoad/Writer/Json.php
+++ src/main/Qafoo/Bsoad/Writer/Json.php
@@ -7,13 +7,25 @@
  */
 
 namespace Qafoo\Bsoad\Writer;
+use Qafoo\Bsoad\Writer;
+use Qafoo\Bsoad\Struct;
 
 /**
  * JSON output writer
  *
  * @version $Revision$
  */
-class Json
+class Json extends Writer
 {
+    /**
+     * Write HTTP interaction
+     *
+     * @param Struct\Interaction $interaction
+     * @return void
+     */
+    public function write( Struct\Interaction $interaction )
+    {
+        echo json_encode( $interaction ), PHP_EOL;
+    }
 }
 
